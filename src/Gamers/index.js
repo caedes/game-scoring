26b8f253caf2bf6b3fb2feeb4cf56e1182262cac
caedes <laurentromain@gmail.com>
2020-01-18 18:09:55 +0100

    feat: add Gamers list

--- src/Gamers/index.js
+++ src/Gamers/index.js
@@ -1,3 +1,6 @@
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 
@@ -5,6 +8,14 @@ export default function Gamers() {
   return (
     <div>
       <Typography variant="h4">Gamers</Typography>
+      <List component="nav">
+        <ListItem>
+          <ListItemText primary="Julia" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Romain" />
+        </ListItem>
+      </List>
     </div>
   );
 }
