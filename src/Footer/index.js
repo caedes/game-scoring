1ed67dd614c3857c37740fe464c6a830387ee4df
caedes <laurentromain@gmail.com>
2020-07-28 22:22:34 +0200

    refactor: 💡 bump Prettier to v2

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,24 +1,24 @@
-import { Link, useLocation } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import GroupIcon from '@material-ui/icons/Group';
-import HistoryIcon from '@material-ui/icons/History';
-import HomeIcon from '@material-ui/icons/Home';
-import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import GroupIcon from "@material-ui/icons/Group";
+import HistoryIcon from "@material-ui/icons/History";
+import HomeIcon from "@material-ui/icons/Home";
+import React from "react";
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
-    position: 'absolute',
+    width: "100%",
+    position: "absolute",
     bottom: 0,
-    borderTop: '1px solid #f1f1f1',
+    borderTop: "1px solid #f1f1f1",
   },
 });
 
 export default function Footer() {
   const currentLocation = useLocation();
-  const footerLocations = ['/', '/history', '/gamers'];
+  const footerLocations = ["/", "/history", "/gamers"];
   const currentLocationIndex = footerLocations.indexOf(
     currentLocation.pathname
   );
