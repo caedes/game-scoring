33b7551fc89cc04f12f298fa5edba95c5a092e04
caedes <laurentromain@gmail.com>
2020-08-04 16:29:27 +0200

    refactor: 💡 move GamersNewScreen styles

--- src/GamersNewScreen/index.js
+++ src/GamersNewScreen/index.js
@@ -1,29 +1,8 @@
-import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 
-const useStyles = makeStyles((theme) => ({
-  stack: {
-    position: "absolute",
-    top: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "white",
-  },
-  form: {
-    "& .MuiTextField-root": {
-      marginTop: theme.spacing(3),
-      marginLeft: theme.spacing(4),
-      width: "80%",
-    },
-  },
-  buttonGroup: {
-    position: "absolute",
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}));
+import useStyles from "./styles";
 
 export default function GamersNewScreen({ headerComponent }) {
   const classes = useStyles();
